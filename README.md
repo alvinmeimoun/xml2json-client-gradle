12f4bbcd57233ad897f40f0a8115a85310b60246
Alvin Meimoun <alvin.meimoun@gmail.com>
2015-11-14 11:52:51 +0100

    chore(build): Add Gradle task convert

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # xml2json-client-gradle
-A simple XML to JSON converter command line tools builded with gradle
+XML to JSON converter
+
+Written in Java 8 and builded with Gradle
+
+
+Convert from a XML file to a JSON file
+gradlew clean build convert -PsrcFile={/path/to/source/file} -PdestFile={/path/to/destination/file}
---
+++ src/main/java/net/alvinmeimoun/xml2json/Xml2JsonMain.java
@@ -0,0 +1,11 @@
+package net.alvinmeimoun.xml2json;
+
+public class Xml2JsonMain {
+
+    public static void main(String[] args){
+        System.out.println("This a test text, feature will be implemented. Please wait and take a cofee :)");
+        System.out.println("\nSource file path : "+args[0]);
+        System.out.println("\nDestination file path : "+args[1]);
+    }
+
+}
